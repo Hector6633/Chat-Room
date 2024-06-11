e23c4ef2170aac275511535b24dfe8ecab127efa
Rajeev <rajeevlakshmanan003@gmail.com>
2024-06-11 21:11:17 +0530

    User account management successfully developed and tested with multiple users.

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('log_in/', views.log_in, name='log_in'),
+    path('logout/', views.sign_out, name='logout'),
    
 ]
